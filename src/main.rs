4f42c3eb7f428498c9cb5a7ef8fa0ae7c93078b7
Jahangir Anwari <jahangir@tarkalabs.com>
2023-06-23 16:36:24 -0300

    Add example route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([0, 0, 0, 0], 3000))
+        .await;
 }
