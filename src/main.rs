d144c75900ac8d9e73295be7717d0d8abc0114ec
Jahangir Anwari <jahangir@tarkalabs.com>
2023-06-23 16:40:54 -0300

    Add root path handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
-
-    warp::serve(hello)
-        .run(([0, 0, 0, 0], 3000))
-        .await;
+    let hello = warp::path::end().map(|| "Hello, World!");
+    warp::serve(hello).run(([0, 0, 0, 0], 3000)).await;
 }
