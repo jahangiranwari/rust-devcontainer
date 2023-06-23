dbd0d095395f1bea1fdbb6396ac00cf2b101761c
Jahangir Anwari <jahangir@tarkalabs.com>
2023-06-23 16:55:44 -0300

    Add println to test debugger

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    let message = "Application is booting up.. 🚀";
+    println!("{}", message);
+
     let hello = warp::path::end().map(|| "Hello, World!");
     warp::serve(hello).run(([0, 0, 0, 0], 3000)).await;
 }
